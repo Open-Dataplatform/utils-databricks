fb2260e276a1c8b7537d1843e5f15dd27c0b4a53
jensrix <jensrix@gmail.com>
2022-02-21 13:17:34 +0000

    Corrected error

--- adf/parameters.py
+++ adf/parameters.py
@@ -8,7 +8,7 @@ def get(parameter_name, default_value, dbutils):
     """Returns parameter from ADF"""
     try:
         value = dbutils.widgets.get(parameter_name)
-    except Py4JJavaError:  # If parameter does not exist
+    except:  # If parameter does not exist
         value = default_value
 
     return value
