71d8ecd3f38ccb648057b19c6723c25e75f3b43b
jensrix <jensrix@gmail.com>
2022-02-21 12:25:59 +0000

    Refactored code

---
+++ adf/parameters.py
@@ -0,0 +1,14 @@
+"""
+Contains functionality to get parameters from ADF
+"""
+
+from py4j.protocol import Py4JJavaError
+
+def get(parameter_name, default_value, dbutils):
+    """Returns parameter from ADF"""
+    try:
+        value = dbutils.widgets.get(parameter_name)
+    except Py4JJavaError:  # If parameter does not exist
+        value = default_value
+
+    return value
