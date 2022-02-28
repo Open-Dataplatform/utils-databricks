6fbf372621874c6cd9b3e03b506b868697a40572
Jens Rix <48241757+jensrix@users.noreply.github.com>
2022-02-28 19:51:28 +0100

    Egress load optimization (#6)
    * Added new functions to read from egress
    
    * Bug fix
    
    * Bug fix
    
    * Removed depricated functions
    
    * Fixed bug
    
    * Changed order of input parameters in function

--- src/custom_utils/dp_storage/writer.py
+++ src/custom_utils/dp_storage/writer.py
@@ -9,34 +9,6 @@ from pyspark.sql.utils import AnalysisException
 ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 
 
-def _get_from_and_to_data_coverage(df_spark, timestamp_column, time_resolution):
-    """Extracts time interval that the input dataframe covers. For time_resolution='year' e.g., the interval will be in
-    full years. 1 second is subtracted from to_date so that the datetime is inside the year/month/day.
-    Example for time_resolution='month': If df_spark has a week of data in January 2021, the output would be
-                                         from_date = datetime(2021, 1, 1) and
-                                         to_date = datetime(2021, 1, 31, 23, 59, 59)"""
-    from_date, to_date = df_spark \
-                             .withColumn('_datetime', F.to_timestamp(timestamp_column, ISO_FORMAT)) \
-                             .select(F.min('_datetime'), F.max('_datetime')) \
-                             .first()
-    
-    if time_resolution == 'year':
-        from_date = datetime(from_date.year, 1, 1)
-        to_date = datetime(to_date.year + 1, 1, 1) - timedelta(seconds=1)
-    elif time_resolution == 'month':
-        from_date = datetime(from_date.year, from_date.month, 1)
-        to_date = to_date + relativedelta(months=1)
-        to_date = datetime(to_date.year, to_date.month, 1) - timedelta(seconds=1)
-    elif time_resolution == 'day':
-        from_date = datetime(from_date.year, from_date.month, from_date.day)
-        to_date = to_date + timedelta(days=1)
-        to_date = datetime(to_date.year, to_date.month, to_date.day) - timedelta(seconds=1)
-    else:
-        raise Exception(f"The input {time_resolution = } is invalid!")
-
-    return from_date, to_date
-
-
 def add_time_columns(df_spark, timestamp_column, time_resolution):
     """Adds year, month, and day columns depending on the input time_resolution."""
     if time_resolution == 'year':
@@ -69,34 +41,48 @@ def add_time_columns(df_spark, timestamp_column, time_resolution):
     return df_spark
 
 
-def read_all_egress_data(egress_identifier, mount_point, spark, schema: StructType):
-    """Returns all egress data."""
+def _get_covered_partitions(df, time_resolution):
+    """Looks up all combinations of year, month, day (depending on time_resolution) in df.
+    Returns list with rows (pyspark.sql.types.Row)."""
+    partition_names = _get_partition_name_list(time_resolution)
+
+    df_covered_partitions = df.select(*partition_names).distinct()
+    partition_list = list(df_covered_partitions.toLocalIterator())
+    return partition_list
+
+
+def get_covered_partition_paths(df, time_resolution):
+    """Generates relative paths to all partitons covered in df.
+    """
+    partition_list = _get_covered_partitions(df, time_resolution)
+
+    if time_resolution == 'year':
+        partition_paths = [f'year={p.year}' for p in partition_list]
+    elif time_resolution == 'month':
+        partition_paths = [f'year={p.year}/month={int(p.month):02}' for p in partition_list]
+    elif time_resolution == 'day':
+        partition_paths = [f'year={p.year}/month={int(p.month):02}/day={int(p.day):02}' for p in partition_list]
+    return partition_paths
+
+
+def read_egress_data_that_overlaps_with_new_data(df_egress_new, egress_identifier, time_resolution, mount_point, spark):
+    """Reads data from all the partitions in egress that overlap with df_egress_new."""
+
+    partition_paths = get_covered_partition_paths(df_egress_new, time_resolution)
+
+    base_path = f'{mount_point}/{egress_identifier}'
+    egress_paths = [f'{base_path}/{path}' for path in partition_paths]
     try:
-        df_egress = spark.read.parquet(f'{mount_point}/{egress_identifier}')
+        df_egress = spark.read.option("basePath", base_path).parquet(*egress_paths)
     except AnalysisException:  # Path does not exist
         # Define empty dataframe with same schema as Ingress data
-        df_egress = spark.createDataFrame((), schema)
+        df_egress = spark.createDataFrame((), df_egress_new.schema)
     return df_egress
 
 
-def read_egress_data_that_overlaps_with_new_data(egress_identifier, df_egress_new, timestamp_column, time_resolution, mount_point, spark):
-    """Returns the egress data that overlaps (in years, months, or days depending on time_resolution) with the input df_egress_new."""
-    df_egress_existing = read_all_egress_data(egress_identifier, mount_point, spark, df_egress_new.schema)
-
-    # TODO: Make this more efficient by selecting on year, month, day columns
-    from_date, to_date = _get_from_and_to_data_coverage(df_egress_new, timestamp_column, time_resolution)
-    df_egress_existing = (
-        df_egress_existing
-        .withColumn('_datetime', F.to_timestamp(timestamp_column, ISO_FORMAT))
-        .where(F.col('_datetime').between(from_date, to_date))
-        .drop('_datetime')
-    )
-    return df_egress_existing
-
-
-def _merge_with_existing_egress(df_egress_new, egress_identifier, timestamp_column, index_columns, time_resolution, mount_point, spark):
+def _merge_with_existing_egress(df_egress_new, egress_identifier, index_columns, time_resolution, mount_point, spark):
     """Reads existing Egress data and merges it with df_egress_new."""
-    df_egress_existing = read_egress_data_that_overlaps_with_new_data(egress_identifier, df_egress_new, timestamp_column, time_resolution, mount_point, spark)
+    df_egress_existing = read_egress_data_that_overlaps_with_new_data(df_egress_new, egress_identifier, time_resolution, mount_point, spark)
 
     df_merged = (
         df_egress_new
@@ -121,10 +107,11 @@ def _get_partition_name_list(time_resolution):
     return partition_list
 
 
-def merge_and_upload_egress_data(df_egress_new, egress_identifier, timestamp_column, index_columns, time_resolution, mount_point, spark):
+def merge_and_upload(df_egress_new, egress_identifier, timestamp_column, index_columns, time_resolution, mount_point, spark):
     """Reads existing Egress data, merges with the new data and uploads."""
+
     df_egress_new = add_time_columns(df_egress_new, timestamp_column, time_resolution)
-    df_egress = _merge_with_existing_egress(df_egress_new, egress_identifier, timestamp_column, index_columns, time_resolution, mount_point, spark)
+    df_egress = _merge_with_existing_egress(df_egress_new, egress_identifier, index_columns, time_resolution, mount_point, spark)
 
     partition_name_list = _get_partition_name_list(time_resolution)
 
