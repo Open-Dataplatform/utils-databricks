cc3934065ecd09511343c0f179c9d246acf01efa
jensrix <jensrix@gmail.com>
2022-05-18 12:09:28 +0000

    Changed so that configs come from input dict instead of widget. Added account to ADLS datasets.

--- src/custom_utils/adf.py
+++ src/custom_utils/adf.py
@@ -12,10 +12,19 @@ def get_parameter(dbutils, parameter_name, default_value=''):
     return dbutils.widgets.get(parameter_name)
 
 
-def get_config_parameter(dbutils, parameter_name):
-    """Gets parameter and converts it to a dict."""
-    json_str_config = get_parameter(dbutils, parameter_name, DEFAULT_CONFIG_PARAMETER)
-    return json.loads(json_str_config)
+def get_config_parameter(dbutils, parameter_name, default_config: dict):
+    """Gets the config and converts it to a dict. If ran in Databricks, default_config is used. If executed from ADF,
+    it is checked whether the config from ADF is the same as default_config."""
+
+    try:
+        json_str_config = dbutils.widgets.get(parameter_name)
+        config = json.loads(json_str_config)
+    except:
+        config = default_config
+
+    assert config == default_config, f'The config from ADF is not the same as the default config for {parameter_name}. From ADF: {config}. Expected: {default_config}'
+
+    return config
 
 
 def initialize_config_widgets(dbutils):
@@ -24,17 +33,17 @@ def initialize_config_widgets(dbutils):
     dbutils.widgets.text('DestinationConfig', DEFAULT_CONFIG_PARAMETER)
 
 
-def get_source_config(dbutils):
+def get_source_config(dbutils, default_source_config):
     """Gets the source configuration and verifies it."""
-    source_config = get_config_parameter(dbutils, 'SourceConfig')
+    source_config = get_config_parameter(dbutils, 'SourceConfig', default_source_config)
     _verify_config(source_config)
 
     return source_config
 
 
-def get_destination_config(dbutils):
+def get_destination_config(dbutils, default_destination_config):
     """Gets the destination configuration and verifies it."""
-    destination_config = get_config_parameter(dbutils, 'DestinationConfig')
+    destination_config = get_config_parameter(dbutils, 'DestinationConfig', default_destination_config)
 
     if len(destination_config) > 1:
         raise Exception('You are only allowed to have one destination dataset.')
@@ -47,10 +56,8 @@ def get_destination_config(dbutils):
 def _verify_config(config):
     """Runs through the dataset configs in a source/destination config and checks the schema.
 
-    :param config:  An example could be {"<dataset_identifier>": {"type":"adls","dataset":"<dataset_name>","container":"landing"}}
+    :param config:  An example could be {"<dataset_identifier>": {"type":"adls", "dataset":"<dataset_name>", "container":"landing", "account":"dplandingstorage"}}
     """
-    if 'EXAMPLE' in config:
-        raise Exception("It looks like you are using an example configuration. If developing in Databricks, manually fill out the source and destination configuration widgets and run the notebook again.")
 
     for dataset_config in config.values():
         _verify_dataset_config(dataset_config)
@@ -59,12 +66,13 @@ def _verify_config(config):
 def _verify_dataset_config(dataset_config):
     """Check the schema of a dataset config
 
-    :param dataset_config:  An example could be {"type":"adls","dataset":"<dataset_name>","container":"landing"}
+    :param dataset_config:  An example could be {"type":"adls", "dataset":"<dataset_name>", "container":"landing", "account":"dplandingstorage"}
     """
     assert 'type' in dataset_config
 
     if dataset_config['type'] == 'adls':
         assert 'dataset' in dataset_config
         assert 'container' in dataset_config
+        assert 'account' in dataset_config
     else:
         raise Exception(f'{dataset_config["type"]} is not a valid dataset type.')
