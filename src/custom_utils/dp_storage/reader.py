85d281f192fcdc55dd9fddf72c5705a623c6aeb7
jensrix <jensrix@gmail.com>
2022-05-19 12:03:15 +0000

    Adapted dp_storage.reader to new configs

--- src/custom_utils/dp_storage/reader.py
+++ src/custom_utils/dp_storage/reader.py
@@ -3,18 +3,19 @@
 import os
 
 
-def get_path_to_triggering_file(mount_point, folder_path, filename, config_for_triggered_dataset):
+def get_path_to_triggering_file(folder_path, filename, config_for_triggered_dataset):
     """Returns path to file that triggered a storage event in Azure.
 
-    :param mount_point:     The mount point returned from the functions in connector.py
     :param folder_path:     @triggerBody().folderPath from ADF
     :param filename:        @triggerBody().fileName from ADF
+    :param config_for_triggered_dataset:    Example: {"type": "adls", "dataset": "<dataset_name>", "container": "landing",
+                                                      "account": "dplandingstorage", "mount_point": "/mnt/dplandingstoragetest"}.
     """
 
     verify_source_path_and_source_config(folder_path, config_for_triggered_dataset)
 
     directory = '/'.join(folder_path.split('/')[1:])  # Remember that folderPath from an ADF trigger has the format "<container>/<directory>"
-    file_path = os.path.join(mount_point, directory, filename)
+    file_path = os.path.join(config_for_triggered_dataset['mount_point'], directory, filename)
 
     return file_path
 
