dcc8471247fc99ea6694b83334d64c81a87c444d
Jens Rix <48241757+jensrix@users.noreply.github.com>
2022-05-23 12:52:08 +0200

    Feature/landing to uniform functionality (#14)
    * Changed so that configs come from input dict instead of widget. Added account to ADLS datasets.
    
    * Removed function to initialize configuration widgets.
    
    * Added functions to mount and unmount multiple storage accounts/containers
    
    * Bug fix
    
    * Bug fix
    
    * Fixed bug with wrong inputs
    
    * Bug fix
    
    * Adapted dp_storage.reader to new configs
    
    * Removed hard check between default and ADF configs
    
    * Renamed writer.py since it only related to the data lake
    
    * Changed function to internal functions
    
    * Added initial take on standardization notebook
    
    * Added writer functions
    
    * Removed writer functions related to old setup
    
    * Changed import structure in template
    
    * Added typing
    
    * Fixed typing bug
    
    * Implemented changes suggested by runethorbek
    
    * Changed import method since it didn't work in Databricks
    
    * Removed transformation template

--- src/custom_utils/adf.py
+++ src/custom_utils/adf.py
@@ -3,38 +3,35 @@
 import json
 
 
-DEFAULT_CONFIG_PARAMETER = '{"EXAMPLE":{"type":"adls","dataset":"EXAMPLE","container":"EXAMPLE"}}'
-
-
-def get_parameter(dbutils, parameter_name, default_value=''):
+def get_parameter(dbutils, parameter_name: str, default_value='') -> str:
     """Creates a text widget and gets parameter value. If ran from ADF, the value is taken from there."""
     dbutils.widgets.text(parameter_name, default_value)
     return dbutils.widgets.get(parameter_name)
 
 
-def get_config_parameter(dbutils, parameter_name):
-    """Gets parameter and converts it to a dict."""
-    json_str_config = get_parameter(dbutils, parameter_name, DEFAULT_CONFIG_PARAMETER)
-    return json.loads(json_str_config)
+def get_config_parameter(dbutils, parameter_name: str, default_config: dict) -> dict:
+    """Gets the config parameter from ADF and converts it to a dict. If ran in Databricks, default_config is used."""
+
+    if is_executed_by_adf(dbutils):
+        json_str_config = dbutils.widgets.get(parameter_name)
+        config = json.loads(json_str_config)
+    else:
+        config = default_config
+
+    return config
 
 
-def initialize_config_widgets(dbutils):
-    """Initializes text widgets for source and destination configs."""
-    dbutils.widgets.text('SourceConfig', DEFAULT_CONFIG_PARAMETER)
-    dbutils.widgets.text('DestinationConfig', DEFAULT_CONFIG_PARAMETER)
-
-
-def get_source_config(dbutils):
+def get_source_config(dbutils, default_source_config: dict) -> dict:
     """Gets the source configuration and verifies it."""
-    source_config = get_config_parameter(dbutils, 'SourceConfig')
+    source_config = get_config_parameter(dbutils, 'SourceConfig', default_source_config)
     _verify_config(source_config)
 
     return source_config
 
 
-def get_destination_config(dbutils):
+def get_destination_config(dbutils, default_destination_config: dict) -> dict:
     """Gets the destination configuration and verifies it."""
-    destination_config = get_config_parameter(dbutils, 'DestinationConfig')
+    destination_config = get_config_parameter(dbutils, 'DestinationConfig', default_destination_config)
 
     if len(destination_config) > 1:
         raise Exception('You are only allowed to have one destination dataset.')
@@ -44,27 +41,34 @@ def get_destination_config(dbutils):
     return destination_config
 
 
-def _verify_config(config):
+def _verify_config(config: dict):
     """Runs through the dataset configs in a source/destination config and checks the schema.
 
-    :param config:  An example could be {"<dataset_identifier>": {"type":"adls","dataset":"<dataset_name>","container":"landing"}}
+    :param config:  An example could be {"<dataset_identifier>":
+                                            {"type": "adls", "dataset": "<dataset_name>", "container": "landing", "account": "dplandingstorage"}
+                                        }
     """
-    if 'EXAMPLE' in config:
-        raise Exception("It looks like you are using an example configuration. If developing in Databricks, manually fill out the source and destination configuration widgets and run the notebook again.")
 
     for dataset_config in config.values():
         _verify_dataset_config(dataset_config)
 
 
-def _verify_dataset_config(dataset_config):
+def _verify_dataset_config(dataset_config: dict):
     """Check the schema of a dataset config
 
-    :param dataset_config:  An example could be {"type":"adls","dataset":"<dataset_name>","container":"landing"}
+    :param dataset_config:  An example could be {"type": "adls", "dataset": "<dataset_name>", "container": "landing", "account": "dplandingstorage"}
     """
     assert 'type' in dataset_config
 
     if dataset_config['type'] == 'adls':
         assert 'dataset' in dataset_config
         assert 'container' in dataset_config
+        assert 'account' in dataset_config
     else:
-        raise Exception(f'{dataset_config["type"]} is not a valid dataset type.')
+        raise ValueError(f'{dataset_config["type"]} is not a valid dataset type.')
+
+
+def is_executed_by_adf(dbutils):
+    """Checks whether notebook is executed by ADF"""
+    is_ran_manually_from_databricks = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get().endswith("@energinet.dk")
+    return not is_ran_manually_from_databricks
--- src/custom_utils/dp_storage/connector.py
+++ src/custom_utils/dp_storage/connector.py
@@ -1,58 +1,67 @@
 """Code to mount storage to the Databricks file system"""
 
 from datetime import datetime
+from typing import Tuple, List
+import logging
 
-def _get_environment(dbutils):
-    try:
+from ..adf import is_executed_by_adf
+
+
+logger = logging.getLogger(__name__)
+
+
+def _get_environment(dbutils) -> str:
+    """If executed from ADF, it reads the environment parameter."""
+    if is_executed_by_adf(dbutils):
         env = dbutils.widgets.get('environment')
-    except:
-        print("Environment is not set. Defaulting to test.")
+        assert (env == 'prod') or (env == 'test'), f"The environment parameter in ADF should be either 'test' or 'prod'. It was {env = }."
+    else:
         env = 'test'
 
     return env
 
 
-def _generate_test_mount_point():
-    return "/mnt/dp_test"
+def _generate_test_mount_point(storage_account: str) -> str:
+    return f'/mnt/{storage_account}test'
 
 
-def _generate_prod_mount_point(dbutils):
+def _generate_prod_mount_point(dbutils, storage_account: str) -> str:
+    """Generates unique mount point path."""
     timestamp = datetime.strftime(datetime.utcnow(), '%y%m%dT%H%M%SZ')
     notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
     notebook_name = notebook_path.split('/')[-1]
 
-    mount_point = f"/mnt/dp_prod_{timestamp}_{notebook_name}"
+    mount_point = f'/mnt/{storage_account}prod_{timestamp}_{notebook_name}'
 
     return mount_point
 
 
-def _get_mount_point(dbutils):
+def _get_mount_point(dbutils, storage_account: str) -> str:
     env = _get_environment(dbutils)
 
     if env == 'test':
-        mount_point = _generate_test_mount_point()
+        mount_point = _generate_test_mount_point(storage_account)
     elif env == 'prod':
-        mount_point = _generate_prod_mount_point(dbutils)
+        mount_point = _generate_prod_mount_point(dbutils, storage_account)
     else:
-        raise Exception(f'The environment {env =  } is invalid. It should be either "test" or "prod"!')
+        raise ValueError(f'The environment ({env =  }) is invalid. It should be either "test" or "prod"!')
 
     return mount_point
 
 
-def _is_test_mounted(dbutils):
-    mount_point = _generate_test_mount_point()
+def _is_test_mounted(dbutils, storage_account: str) -> bool:
+    mount_point = _generate_test_mount_point(storage_account)
     return any(mount.mountPoint == mount_point for mount in dbutils.fs.mounts())
 
 
-def _construct_config_and_mount(dbutils):
+def _do_mount(dbutils, storage_account: str, container: str) -> str:
     """Checks environment, reads service principals, and mounts."""
     env = _get_environment(dbutils)
 
     tenant_id = dbutils.secrets.get(scope="shared-key-vault",key="tenantid")
     client_id = dbutils.secrets.get(scope="shared-key-vault",key=f"clientid-databricks-sp-{env}")
     client_secret =  dbutils.secrets.get(scope="shared-key-vault",key=f"pwd-databricks-sp-{env}")
-    account_name = dbutils.secrets.get(scope="shared-key-vault",key=f"accountname-storage-{env}")
-    container = "datasets"
+    account_name = f'{storage_account}{env}'
 
     configs = {"fs.azure.account.auth.type": "OAuth",
                "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
@@ -60,33 +69,89 @@ def _construct_config_and_mount(dbutils):
                "fs.azure.account.oauth2.client.secret": client_secret,
                "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
 
-    mount_point = _get_mount_point(dbutils)
+    mount_point = _get_mount_point(dbutils, storage_account)
 
     dbutils.fs.mount(source=f"abfss://{container}@{account_name}.dfs.core.windows.net/",
                      mount_point=mount_point,
                      extra_configs=configs)
 
-    print(f"Mount point ({mount_point = }) is ready")
+    logger.info(f"Mount point ({mount_point = }) is ready")
     return mount_point
 
 
-def mount(dbutils):
-    """Mounts storage to /mnt/dp_{env} if it is not yet mounted. Returns mount point."""
+def mount(dbutils, source_config: dict, destination_config: dict) -> Tuple[dict, dict]:
+    """Mounts storage for all ADLS datasets in both source and destination. Returns configs with 'mount_point' added to the
+    individual dataset config dicts."""
+
+    containers_to_mount = _list_containers_to_mount(source_config, destination_config)
+    mount_point_dict = _mount_all_containers(dbutils, containers_to_mount)
+
+    source_config = _add_mount_points_to_config(source_config, mount_point_dict)
+    destination_config = _add_mount_points_to_config(destination_config, mount_point_dict)
+
+    return source_config, destination_config
+
+
+def _add_mount_points_to_config(config, mount_point_dict: dict) -> dict:
+    """Adds mount points to every dataset config in the input config"""
+    for data_config in config.values():
+        if data_config['type'] == 'adls':
+            data_config['mount_point'] = mount_point_dict[(data_config['account'], data_config['container'])]
+
+    return config
+
+
+def _mount_all_containers(dbutils, containers_to_mount: List[tuple]) -> dict:
+    """"Mounts all containers and returns dictionary with {(<account>, <container>): <mount_points>, ...}."""
     env = _get_environment(dbutils)
 
-    # If running in test environment and test storage is already mounted.
-    if (env == 'test') and _is_test_mounted(dbutils):
-        mount_point = _generate_test_mount_point()
-    else:
-        print("Mounting...")
-        mount_point = _construct_config_and_mount(dbutils)
+    mount_points = {}
+    for storage_account, container in containers_to_mount:
 
-    return mount_point
+        # If running in test environment and test storage is already mounted.
+        if (env == 'test') and _is_test_mounted(dbutils, storage_account):
+            mount_point = _generate_test_mount_point(storage_account)
+        else:
+            logger.info("Mounting storage account %s and container %s", storage_account, container)
+            mount_point = _do_mount(dbutils, storage_account, container)
+
+        mount_points[(storage_account, container)] = mount_point
+
+    return mount_points
 
 
-def unmount_if_prod(mount_point, dbutils):
+def _list_containers_to_mount(source_config, destination_config: dict) -> List[tuple]:
+    """Returns list with ("<account>", "<container>") for all combinations of accounts/containers in the input configs."""
+
+    containers_to_mount = set()
+
+    dataset_configs = list(source_config.values()) + list(destination_config.values())
+    for data_config in dataset_configs:
+        if data_config['type'] == 'adls':
+            containers_to_mount.add((data_config['account'], data_config["container"]))
+
+    return list(containers_to_mount)
+
+
+def _list_mount_points(source_config, destination_config: dict) -> List[str]:
+    """Returns list with all mount points."""
+
+    containers_to_mount = set()
+
+    combined_configs = {**source_config, **destination_config}
+    for data_config in combined_configs.values():
+        if data_config['type'] == 'adls':
+            containers_to_mount.add(data_config['mount_point'])
+
+    return list(containers_to_mount)
+
+
+def unmount_if_prod(dbutils, source_config: dict, destination_config: dict):
     """If running in prod, unmount storage"""
     env = _get_environment(dbutils)
 
     if env == 'prod':
-        dbutils.fs.unmount(mount_point)
+        mount_points = _list_mount_points(source_config, destination_config)
+        for mount_point in mount_points:
+            dbutils.fs.unmount(mount_point)
+            logger.info(f"Successfully unmounted mount_point = %s", mount_point)
