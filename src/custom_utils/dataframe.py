71e33434726e891112bf836cb5c59e5ccfe657e3
xazms <xazms@energinet.dk>
2024-08-19 10:16:33 +0200

    Update dataframe.py

--- src/custom_utils/dataframe.py
+++ src/custom_utils/dataframe.py
@@ -1,10 +1,7 @@
-"""Functions to modify Pyspark dataframes"""
-
 from typing import List
 from pyspark.sql.types import ArrayType, StructType, StringType
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame
-
 
 def _get_array_and_struct_columns(df):
     """Return list with columns (names and types) of either ArrayType or StructType"""
@@ -16,11 +13,10 @@ def _get_array_and_struct_columns(df):
 
     return complex_columns
 
-
 def _get_expanded_columns_with_aliases(df, column_name, layer_separator='_'):
     """Return list of nested columns (pyspark.sql.column.Column) in a column struct. To be used in df.select().
 
-    layer_separator is used as the seperator in the new column name between the column and nested column names.
+    layer_separator is used as the separator in the new column name between the column and nested column names.
     """
     expanded_columns = []
     for nested_column in df.select(f'`{column_name}`.*').columns:
@@ -31,23 +27,16 @@ def _get_expanded_columns_with_aliases(df, column_name, layer_separator='_'):
 
     return expanded_columns
 
-
 def flatten_array_column(df, column_name):
     """Return dataframe with flattened array column."""
-    df = df.withColumn(column_name, F.explode_outer(column_name))
+    df = df.withColumn(column_name, F.explode_outer(F.col(column_name)))
     return df
-
 
 def flatten_struct_column(df, column_name, layer_separator='_'):
     """Return dataframe with flattened struct column."""
-
     expanded_columns = _get_expanded_columns_with_aliases(df, column_name, layer_separator)
-
-    df = df.select("*", *expanded_columns) \
-           .drop(column_name)
-
+    df = df.select("*", *expanded_columns).drop(column_name)
     return df
-
 
 def flatten(df, layer_separator='_'):
     """Return dataframe with flattened arrays and structs.
@@ -68,7 +57,6 @@ def flatten(df, layer_separator='_'):
         complex_columns = _get_array_and_struct_columns(df)
 
     return df
-
 
 def flatten_df(df: DataFrame, depth_level=None, current_level=0, max_depth=1, type_mapping: dict = None) -> DataFrame:
     """
@@ -99,10 +87,10 @@ def flatten_df(df: DataFrame, depth_level=None, current_level=0, max_depth=1, ty
     while complex_fields:
         for col_name, data_type in complex_fields.items():
             if current_level + 1 == depth_level:
-                df = df.withColumn(col_name, to_json(F.col(col_name)))
+                df = df.withColumn(col_name, F.to_json(F.col(col_name)))
             else:
                 if isinstance(data_type, ArrayType):
-                    df = df.withColumn(col_name, explode_outer(F.col(col_name)))
+                    df = df.withColumn(col_name, F.explode_outer(F.col(col_name)))
                 if isinstance(data_type, StructType):
                     expanded = [F.col(f"{col_name}.{k}").alias(f"{col_name}_{k}") for k in data_type.fieldNames()]
                     df = df.select("*", *expanded).drop(col_name)
@@ -119,7 +107,6 @@ def flatten_df(df: DataFrame, depth_level=None, current_level=0, max_depth=1, ty
 
     return df
 
-
 def _string_replace(s: str, replacements: dict):
     """Return string with multiple replacements."""
     for string_before, string_after in replacements.items():
@@ -127,13 +114,11 @@ def _string_replace(s: str, replacements: dict):
 
     return s
 
-
 def rename_columns(df, replacements={'.': '_'}):
     """Return dataframe with columns renamed according to replacement dict."""
     for column_name in df.columns:
         df = df.withColumnRenamed(column_name, _string_replace(column_name, replacements))
     return df
-
 
 def rename_and_cast_columns(df, column_mapping=None, cast_type_mapping=None):
     """
@@ -159,7 +144,6 @@ def rename_and_cast_columns(df, column_mapping=None, cast_type_mapping=None):
                 df = df.withColumn(col_name, F.col(col_name).cast(new_type))
     return df
 
-
 def add_columns_that_are_not_in_df(df, column_names: List[str]):
     """Add columns in column_names that are not already in dataframe.
 
@@ -171,7 +155,6 @@ def add_columns_that_are_not_in_df(df, column_names: List[str]):
             df = df.withColumn(column_name, F.lit(None))
             print(f'Column "{column_name}" was added.')
     return df
-
 
 def read_json_from_binary(spark, schema, data_file_path):
     """
@@ -205,7 +188,7 @@ def read_json_from_binary(spark, schema, data_file_path):
     df_final_with_filename = df_final_with_filename.drop("json_string", "content", "path", "modificationTime", "length", "id")
 
     # Reorder the columns to have `input_file_name` as the first column
-    columns = ["input_file_name"] + [F.col for F.col in df_final_with_filename.columns if F.col != "input_file_name"]
+    columns = ["input_file_name"] + [col for col in df_final_with_filename.columns if col != "input_file_name"]
     df_final_with_filename = df_final_with_filename.select(columns)
 
     return df_final_with_filename
