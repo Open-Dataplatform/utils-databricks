153fa817a845047994d0f73bc8f20704dfa3175b
xazms <xazms@energinet.dk>
2024-08-22 13:56:10 +0200

    update

--- src/custom_utils/dp_storage/__init__.py
+++ src/custom_utils/dp_storage/__init__.py
@@ -4,6 +4,6 @@ from .reader import *
 from .writer import *
 from .config import Config, initialize_config, initialize_notebook  # Import Config, initialize_config, and initialize_notebook
 
-# Automatically call the setup function when this module is imported
+# Automatically call the initialize_notebook function when this module is imported
 if "dbutils" in globals() and "helper" in globals():
     spark, config = initialize_notebook(dbutils, helper)
