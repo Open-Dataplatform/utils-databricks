c032b8938ce699ad5b234e01c5c067c767d19832
xazms <xazms@energinet.dk>
2024-09-14 13:03:50 +0200

    Update

--- src/custom_utils/config/__init__.py
+++ src/custom_utils/config/__init__.py
@@ -1,5 +1,1 @@
-from .config import Config, initialize_config, initialize_notebook  # Import Config, initialize_config, and initialize_notebook
-
-# Automatically call the initialize_notebook function when this module is imported
-if "dbutils" in globals() and "helper" in globals():
-    spark, config = initialize_notebook(dbutils, helper)
+from .config import Config, initialize_config, initialize_notebook
