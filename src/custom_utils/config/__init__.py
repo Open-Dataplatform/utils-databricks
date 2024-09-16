1e50144cde0934abcc0846d2c8c98242f7bb226b
xazms <xazms@energinet.dk>
2024-09-16 07:49:13 +0200

    Update

--- src/custom_utils/config/__init__.py
+++ src/custom_utils/config/__init__.py
@@ -1,3 +1,9 @@
 # File: custom_utils/config/__init__.py
 
-from .config import Config, initialize_config, initialize_notebook
+from .reader import *
+from .writer import *
+from .config import Config, initialize_config, initialize_notebook  # Import Config, initialize_config, and initialize_notebook
+
+# Automatically call the initialize_notebook function when this module is imported
+if "dbutils" in globals() in globals():
+    spark, config, logger = initialize_notebook(dbutils)
