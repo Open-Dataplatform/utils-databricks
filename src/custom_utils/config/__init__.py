95752558f7a7ab4f5669f6fc74898c50f23c6fce
xazms <xazms@energinet.dk>
2024-09-16 07:51:21 +0200

    Update

--- src/custom_utils/config/__init__.py
+++ src/custom_utils/config/__init__.py
@@ -1,7 +1,5 @@
 # File: custom_utils/config/__init__.py
 
-from .reader import *
-from .writer import *
 from .config import Config, initialize_config, initialize_notebook  # Import Config, initialize_config, and initialize_notebook
 
 # Automatically call the initialize_notebook function when this module is imported
