e945e5c27ef9ec7ba8f1556cdf576da3cb325cf6
xazms <xazms@energinet.dk>
2024-09-17 13:59:07 +0200

    Update

--- src/custom_utils/transformations/dataframe.py
+++ src/custom_utils/transformations/dataframe.py
@@ -3,11 +3,11 @@
 import json
 import pyspark.sql.functions as F
 from typing import Dict, Tuple, List
-from pyspark.sql.types import ArrayType, StructType, StringType, IntegerType, BooleanType, DoubleType, StructField
+from pyspark.sql.types import ArrayType, StructType, StringType
 from pyspark.sql import DataFrame
 from custom_utils.config.config import Config
 from custom_utils.logging.logger import Logger
-from custom_utils.dp_storage import reader  # Ensure reader is correctly imported
+
 
 class Transformer:
     def __init__(self, config: Config, logger: Logger = None, debug: bool = None):
@@ -24,14 +24,35 @@ class Transformer:
         self.debug = debug if debug is not None else config.debug
         self.logger.debug = self.debug
 
-    def flatten_array_column(self, df: DataFrame, column_name: str) -> DataFrame:
+    def _log_block(self, header: str, content_lines: List[str], level="info"):
+        """
+        Logs a block of messages with a header and separators.
+
+        Args:
+            header (str): The header for the log block.
+            content_lines (List[str]): A list of messages to be logged within the block.
+            level (str): The log level for the block (e.g., "info", "error"). Defaults to "info".
+        """
+        if self.debug or level != "info":
+            self.logger.log_message(f"\n=== {header} ===", level=level)
+            self.logger.log_message("------------------------------", level=level)
+            
+            for line in content_lines:
+                self.logger.log_message(line, level=level)
+
+            # End with a separator
+            self.logger.log_message("------------------------------", level=level)
+
+    def _flatten_array_column(self, df: DataFrame, column_name: str) -> DataFrame:
+        """Flattens an array column in the DataFrame using `explode_outer`."""
         try:
             return df.withColumn(column_name, F.explode_outer(F.col(column_name)))
         except Exception as e:
             self.logger.log_message(f"Error flattening array column '{column_name}': {e}", level="error")
             raise RuntimeError(f"Error flattening array column '{column_name}'")
 
-    def flatten_struct_column(self, df: DataFrame, column_name: str, layer_separator: str = "_") -> DataFrame:
+    def _flatten_struct_column(self, df: DataFrame, column_name: str, layer_separator: str = "_") -> DataFrame:
+        """Flattens a struct column in the DataFrame by expanding its nested fields."""
         try:
             expanded_columns = self._get_expanded_columns_with_aliases(df, column_name, layer_separator)
             return df.select("*", *expanded_columns).drop(column_name)
@@ -40,12 +61,14 @@ class Transformer:
             raise RuntimeError(f"Error flattening struct column '{column_name}'")
 
     def _get_expanded_columns_with_aliases(self, df: DataFrame, column_name: str, layer_separator: str = "_") -> List[F.Column]:
+        """Generates a list of nested columns in a struct with appropriate aliases."""
         return [
             F.col(f"`{column_name}`.`{nested_column}`").alias(f"{column_name}{layer_separator}{nested_column}")
             for nested_column in df.select(f"`{column_name}`.*").columns
         ]
 
     def flatten_df(self, df: DataFrame, depth_level: int, max_depth: int, type_mapping: Dict[str, str] = None) -> DataFrame:
+        """Flattens complex fields in a DataFrame up to a specified depth level."""
         try:
             if type_mapping:
                 df = df.select(
@@ -63,7 +86,7 @@ class Transformer:
                     if current_level + 1 == depth_level:
                         df = df.withColumn(col_name, F.to_json(F.col(col_name)))
                     else:
-                        df = self.flatten_array_column(df, col_name) if isinstance(data_type, ArrayType) else self.flatten_struct_column(df, col_name)
+                        df = self._flatten_array_column(df, col_name) if isinstance(data_type, ArrayType) else self._flatten_struct_column(df, col_name)
 
                 current_level += 1
 
@@ -81,7 +104,8 @@ class Transformer:
             self.logger.log_message(f"Error flattening DataFrame: {e}", level="error")
             raise RuntimeError(f"Error flattening DataFrame")
 
-    def read_json_from_binary(self, schema: StructType, data_file_path: str) -> DataFrame:
+    def _read_json_from_binary(self, schema: StructType, data_file_path: str) -> DataFrame:
+        """Reads JSON files as binary, parses the JSON content, and associates the `input_file_name`."""
         try:
             binary_df = self.config.spark.read.format("binaryFile").load(data_file_path)
             df_with_filename = (
@@ -107,6 +131,7 @@ class Transformer:
             raise RuntimeError(f"Error processing binary JSON files")
 
     def _format_schema(self, schema: StructType, indent_level: int = 0) -> str:
+        """Formats the schema of a DataFrame for logging."""
         formatted_schema = ""
         indent = " " * (indent_level * 2)
         for field in schema.fields:
@@ -124,122 +149,37 @@ class Transformer:
                 formatted_schema += f"{indent}|-- {field.name}: {field_type.simpleString()} (nullable = {field.nullable})\n"
         return formatted_schema
 
-    def json_schema_to_spark_struct(self, schema_file_path, definitions=None):
-        """
-        Reads a JSON schema file, parses it, and converts it to a PySpark StructType.
-        """
-        try:
-            with open(schema_file_path, "r") as f:
-                json_schema = json.load(f)
-        except Exception as e:
-            raise ValueError(f"Failed to load or parse schema file '{schema_file_path}': {e}")
-
-        if definitions is None:
-            definitions = json_schema.get("definitions", {})
-
-        def resolve_ref(ref):
-            ref_path = ref.split("/")[-1]
-            return definitions.get(ref_path, {})
-
-        def parse_type(field_props):
-            if isinstance(field_props, list):
-                valid_types = [parse_type(fp) for fp in field_props if fp.get("type") != "null"]
-                return valid_types[0] if valid_types else StringType()
-
-            if "$ref" in field_props:
-                field_props = resolve_ref(field_props["$ref"])
-
-            json_type = field_props.get("type")
-            if isinstance(json_type, list):
-                json_type = next((t for t in json_type if t != "null"), None)
-
-            if json_type == "string":
-                return StringType()
-            elif json_type == "integer":
-                return IntegerType()
-            elif json_type == "boolean":
-                return BooleanType()
-            elif json_type == "number":
-                return DoubleType()
-            elif json_type == "array":
-                items = field_props.get("items")
-                return ArrayType(parse_type(items) if items else StringType())
-            elif json_type == "object":
-                properties = field_props.get("properties", {})
-                return StructType([StructField(k, parse_type(v), True) for k, v in properties.items()])
-            else:
-                return StringType()
-
-        def parse_properties(properties):
-            return StructType([StructField(name, parse_type(props), True) for name, props in properties.items()])
-
-        return json_schema, parse_properties(json_schema.get("properties", {}))
-
-    def get_json_depth(self, json_schema, current_depth=0, definitions=None, logger=None, depth_level=None) -> int:
-        def calculate_depth(schema, current_depth, definitions):
-            if isinstance(schema, dict) and '$ref' in schema:
-                ref_key = schema['$ref'].split('/')[-1]
-                ref_schema = definitions.get(ref_key, {})
-                if ref_schema:
-                    return calculate_depth(ref_schema, current_depth, definitions)
-                else:
-                    raise ValueError(f"Reference '{ref_key}' not found in definitions.")
-
-            max_depth = current_depth
-
-            if isinstance(schema, dict):
-                if 'properties' in schema:
-                    properties_depth = max(calculate_depth(v, current_depth + 1, definitions) for v in schema['properties'].values())
-                    max_depth = max(max_depth, properties_depth)
-
-                if 'items' in schema:
-                    if isinstance(schema['items'], dict):
-                        items_depth = calculate_depth(schema['items'], current_depth + 1, definitions)
-                        max_depth = max(max_depth, items_depth)
-
-            if isinstance(schema, list):
-                list_depths = [calculate_depth(item, current_depth + 1, definitions) for item in schema]
-                max_depth = max(max_depth, *list_depths)
-
-            return max_depth
-
-        max_depth = calculate_depth(json_schema, current_depth, definitions or json_schema.get('definitions', {}))
-
-        if logger:
-            logger.log_message(f"Maximum depth level of the JSON schema: {max_depth}; Flattened depth level of the JSON file: {depth_level}")
-
-        return max_depth
-
     def process_and_flatten_json(self, schema_file_path: str, data_file_path: str, depth_level: int = None, include_schema: bool = False) -> Tuple[DataFrame, DataFrame]:
+        """Orchestrates the JSON processing pipeline, including schema reading, data flattening, and logging."""
         self.logger.log_start("process_and_flatten_json")
         try:
-            schema_json, schema = self.json_schema_to_spark_struct(schema_file_path)
+            schema_json, schema = self.config.json_schema_to_spark_struct(schema_file_path)
             depth_level_to_use = depth_level if depth_level is not None else 1
-            max_depth = self.get_json_depth(schema_json, logger=None, depth_level=depth_level_to_use)
+            max_depth = self.config.get_json_depth(schema_json, depth_level=depth_level_to_use)
 
             start_lines = [
                 f"Schema file path: {schema_file_path}",
                 f"Data file path: {data_file_path}",
                 f"Flattened depth level of the JSON file: {depth_level_to_use}"
             ]
-            self.logger.log_block("Starting Processing", start_lines)
+            self._log_block("Starting Processing", start_lines)
 
             if include_schema:
                 self.logger.log_message(f"Schema JSON:\n{json.dumps(schema_json, indent=4)}", level="info")
 
-            df = self.read_json_from_binary(schema, data_file_path)
+            df = self._read_json_from_binary(schema, data_file_path)
 
             if self.debug:
-                self.logger.log_block("Initial DataFrame Info", [])
+                self._log_block("Initial DataFrame Info", [])
                 initial_row_count = df.count()
                 self.logger.log_message(f"Initial DataFrame row count: {initial_row_count}", level="info")
                 initial_schema_str = self._format_schema(df.schema)
                 self.logger.log_message(f"Initial DataFrame schema:\nroot\n{initial_schema_str}", level="info")
 
-            df_flattened = self.flatten_df(df, depth_level=depth_level_to_use, max_depth=max_depth, type_mapping=reader.get_type_mapping())
+            df_flattened = self.flatten_df(df, depth_level=depth_level_to_use, max_depth=max_depth, type_mapping=self.config.get_type_mapping())
 
             if self.debug:
-                self.logger.log_block("Flattened DataFrame Info", [])
+                self._log_block("Flattened DataFrame Info", [])
                 flattened_row_count = df_flattened.count()
                 self.logger.log_message(f"Flattened DataFrame row count: {flattened_row_count}", level="info")
                 flattened_schema_str = self._format_schema(df_flattened.schema)
