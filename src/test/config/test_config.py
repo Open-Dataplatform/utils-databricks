cf532cf7c07c5b3e34933a9e6df9f6eb084ef8f8
Ajanthan Ketheeswaran Kumarasamy <ajkku@energinet.dk>
2025-05-06 15:15:17 +0200

    Started catalog utils test

--- src/test/config/test_config.py
+++ src/test/config/test_config.py
@@ -1,25 +1,28 @@
 import pytest
-from typing import Any, Callable
+from typing import Any
 
 from ..test_utils import dbutils
 from ...custom_utils import Config
 
 class TestConfig:
-    def setup_method(self, method: Callable):
+    def setup_method(self, method: callable):
         print(f"Setting up {method}")
         self.dbutils: dbutils = dbutils()
         self.debug: bool = False
         self.config = Config(dbutils=self.dbutils, debug=self.debug)
 
-    def teardown_method(self, method: Callable):
+    def teardown_method(self, method: callable):
         print(f"Tearing down {method}")
+        del self.dbutils
+        del self.debug
+        del self.config
     
     def test_initialisation(self):
         with pytest.raises(RuntimeError) as excinfo_initialize:
             Config()
         with pytest.raises(RuntimeError) as excinfo_reinitialize:
             self.config.initialize()
-        assert str(excinfo_initialize.value) == str(excinfo_initialize.value)
+        assert str(excinfo_initialize.value) == str(excinfo_reinitialize.value)
     
     @pytest.mark.parametrize("namespace", [locals(), globals()])
     def test_unpack(self, namespace: dict[str, Any]):
