6a1fd5eca03cf5f37f2774260d33de366764f4bd
Ajanthan Ketheeswaran Kumarasamy <ajkku@energinet.dk>
2025-05-06 15:15:17 +0200

    Started catalog utils test

--- src/test/catalog/test_catalog_utils.py
+++ src/test/catalog/test_catalog_utils.py
@@ -1,7 +1,22 @@
 import pytest
-from typing import Any, Callable
-
+from typing import Any
+from pathlib import Path
+from os import getcwd
+from uuid import uuid4
 from ..test_utils import dbutils
-from ...custom_utils.config.config import Config
+from ...custom_utils import DataStorageManager
 
-class TestData
+class TestDataStorageManager:
+    def setup_method(self, method: callable):
+        print(f"Setting up {method}")
+        self.storage_manager: DataStorageManager = DataStorageManager(logger=None)
+        self.dbutils: dbutils = dbutils()
+        
+    def teardown_method(self, method: callable):
+        print(f"Tearing down {method}")
+        del self.storage_manager
+    
+    def test_ensure_path_exists(self):
+        existing_path: Path = Path(getcwd())
+        #unexisting_path: existing_path/str(uuid4())
+        self.storage_manager.ensure_path_exists(dbutils=self.dbutils,destination_path=str(existing_path))
