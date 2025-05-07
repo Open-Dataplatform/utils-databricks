175d107bfc1f7c26d164a231a872208aad66de2f
Ajanthan Ketheeswaran Kumarasamy <ajkku@energinet.dk>
2025-05-07 17:31:48 +0200

    Add proper CI/CD workflow and catalog utils tests

--- src/test/config/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .. import custom_utils
-
-# from unittest import TestCase
-# from unittest.mock import Mock, patch
--- src/test/logging/test_logger.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-# from ...custom_utils.logging.logger import Logger, LoggerTester
-
-# @pytest.fixture
-# def logger():
-#     return Logger()
-
-# def test_set_level(logger):
-#     pass
