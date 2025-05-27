013ebea40fb06c1f38f333869822dd632c7041da
Ajanthan Ketheeswaran Kumarasamy <ajkku@energinet.dk>
2025-05-27 16:27:53 +0200

    transformation test initialisation

--- test/transformations/dataframe_test.py
+++ test/transformations/dataframe_test.py
@@ -2,7 +2,6 @@ import pytest
 from pathlib import Path
 
 from custom_utils import DataFrameTransformer, Config
-#from ..test_utils import dbutils
 from databricks.sdk.runtime import dbutils
 
 class TestDataFrameTransformer:
@@ -10,25 +9,19 @@ class TestDataFrameTransformer:
         self.temp_path: Path = Path('/mnt')
         
         self.dbutils: dbutils = dbutils
-        self.dbutils.widgets.dropdown("FileType", "json", ["json", "xml", "xlsx"], "File Type")
-
-        # Step 2: Define Source and Destination Storage Accounts
-        self.dbutils.widgets.text("SourceStorageAccount", "dplandingstoragetest", "Source Storage Account")
-        self.dbutils.widgets.text("DestinationStorageAccount", "dpuniformstoragetest", "Destination Storage Account")
-
-        # Step 3: Configure Source Container and Dataset Identifier
-        self.dbutils.widgets.text("SourceContainer", "landing", "Source Container")
-        self.dbutils.widgets.text("SourceDatasetidentifier", "custom_utils_test_data", "Source Datasetidentifier")
-
-        # Step 4: Specify Source File Name and Key Columns
-        self.dbutils.widgets.text("SourceFileName", "custom_utils_test_data*", "Source File Name")
-        self.dbutils.widgets.text("KeyColumns", "uuid", "Key Columns")
-
-        # Step 5: Set Additional Parameters for Feedback Column, Depth Level, Schema Folder
-        self.dbutils.widgets.text("DepthLevel", "", "Depth Level (Leave blank for no limit)")
-        self.dbutils.widgets.text("SchemaFolderName", "schemachecks", "Schema Folder Name")
+        
+        self.dbutils.widgets.dropdown("FileType", "json", ["json", "xml", "xlsx"])
+        self.dbutils.widgets.text("SourceStorageAccount", "dplandingstoragetest")
+        self.dbutils.widgets.text("DestinationStorageAccount", "dpuniformstoragetest")
+        self.dbutils.widgets.text("SourceContainer", "landing")
+        self.dbutils.widgets.text("SourceDatasetidentifier", "custom_utils_test_data")
+        self.dbutils.widgets.text("SourceFileName", "custom_utils_test_data*")
+        self.dbutils.widgets.text("KeyColumns", "A")
+        self.dbutils.widgets.text("DepthLevel", "")
+        self.dbutils.widgets.text("SchemaFolderName", "schemachecks")
             
         self.config = Config(dbutils=self.dbutils)
+        self.config.unpack(globals())
         self.transformer: DataFrameTransformer = DataFrameTransformer(config=self.config)
         
     def teardown_method(self):
@@ -36,12 +29,26 @@ class TestDataFrameTransformer:
         
     def test_process_and_flatten_data(self):
         depth_level: int = 1
+        print(self.dbutils.widgets.get("SourceDatasetidentifier"))
+        print(self.dbutils.widgets.get("SourceContainer"))
         df_init, df_flat = self.transformer.process_and_flatten_data(depth_level=depth_level)
+        print(df_init)
+        print(df_flat)
         assert df_init == 1
 
 if __name__ == "__main__":
     dbutils: dbutils = dbutils
     config: Config = Config(dbutils=dbutils)
+    dbutils.widgets.dropdown("FileType", "json", ["json", "xml", "xlsx"], "File Type")
+    dbutils.widgets.text("SourceStorageAccount", "dplandingstoragetest", "Source Storage Account")
+    dbutils.widgets.text("DestinationStorageAccount", "dpuniformstoragetest", "Destination Storage Account")
+    dbutils.widgets.text("SourceContainer", "landing", "Source Container")
+    dbutils.widgets.text("SourceDatasetidentifier", "custom_utils_test_data", "Source Datasetidentifier")
+    dbutils.widgets.text("SourceFileName", "custom_utils_test_data*", "Source File Name")
+    dbutils.widgets.text("KeyColumns", "uuid", "Key Columns")
+    dbutils.widgets.text("DepthLevel", "", "Depth Level (Leave blank for no limit)")
+    dbutils.widgets.text("SchemaFolderName", "schemachecks", "Schema Folder Name")
+    
     transformer: DataFrameTransformer = DataFrameTransformer(config=config)
     df_init, df_flat = transformer.process_and_flatten_data(depth_level=1)
     print(df_init)
