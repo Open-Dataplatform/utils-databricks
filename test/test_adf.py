ba09ca8fbe69cbcb1aa46bf8252c9f7e9dee56fb
AK <72132881+ajanthank@users.noreply.github.com>
2025-07-18 09:10:26 +0200

    Databricks free (#64)
    * initial commit
    
    * Removing databricks-sdk
    
    * removing git package from requirements_dev.txt
    
    * updating requirements_dev.txt
    
    * updating requirements_dev.txt second time
    
    * updating env
    
    * updating requirements_dev.txt
    
    * testing dbutils
    
    * debugging config test
    
    * debugging config test
    
    * Code cleaning and restructuring
    
    * removing *test_data* from .gitignore
    
    * updating .gitignore
    
    * updating .gitignore
    
    * another update
    
    * setting assertion error is schema is None
    
    * adding test_adf.py
    
    * initial commit
    
    * Removing databricks-sdk
    
    * Code cleaning and restructuring
    
    * removing *test_data* from .gitignore
    
    * updating .gitignore
    
    * updating .gitignore
    
    * another update
    
    * adding test_adf.py
    
    ---------
    
    Co-authored-by: Ajanthan Ketheeswaran Kumarasamy <ajkku@energinet.dk>

--- test/test_adf.py
+++ test/test_adf.py
@@ -1,26 +1,22 @@
 import pytest
-from typing import Any
+from typing import Any, Callable
 from custom_utils.adf import get_parameter
 
 from .test_utils.dbutils_mocker import dbutils_mocker, dbutils
 
-class testadf:
-    
-    def setup_method(self, method: callable):
-        print(f"Setting up {method}")
+class Testadf:
+    def setup_method(self):
         self.dbutils: dbutils_mocker = dbutils
         self.param_name: str = "TestParam"
 
-    def teardown_method(self, method: callable):
-        print(f"Tearing down {method}")
+    def teardown_method(self):
         del self.dbutils
         del self.param_name
 
-    @pytest.mark.parametrize("value", ["test_string", 2, None])
+    @pytest.mark.parametrize("value", ["test_string", str(2), str(None)])
     def test_get_parameter(self, value: Any):
         return_value: Any = get_parameter(dbutils=self.dbutils,
                                           parameter_name=self.param_name,
                                           default_value=value)
         assert self.param_name in self.dbutils.widgets.getAll()
         assert return_value == value
-        
--- test/test_utils/dbutils_mocker/__init__.py
+++ test/test_utils/dbutils_mocker/__init__.py
@@ -1,3 +1,5 @@
 from .dbutils_mocker import dbutils_mocker
 
 dbutils: dbutils_mocker = dbutils_mocker()
+
+__all__: list[str] = ["dbutils_mocker", "dbutils"]
