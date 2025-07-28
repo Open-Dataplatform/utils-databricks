db956b69b923ef4aeecf2d06d51c66d1381c8c6a
andersmarstrandAXM <axm@energinet.dk>
2025-07-28 14:55:56 +0200

    Setting JVM and python connection
    In order to execute unit-test a connection between python and java
    virtual machine is set in this commit.

--- test/test_utils/dbutils_mocker/__init__.py
+++ test/test_utils/dbutils_mocker/__init__.py
@@ -1,4 +1,9 @@
 from .dbutils_mocker import dbutils_mocker
+import os 
+import sys
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 dbutils: dbutils_mocker = dbutils_mocker()
 
